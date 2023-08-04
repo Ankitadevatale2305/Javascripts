f1a915bb988c71a0fee6fc541add9d8d5b3a4ab5
Ankitadevatale2305 <126754673+Ankitadevatale2305@users.noreply.github.com>
2023-08-04 03:59:53 +0000

    string methods

--- Javascript/variable.js
+++ Javascript/variable.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+const name = "Ankita";
+const repoCount = 50;
+//console.log(name + ' ' + repoCount);
+
+console.log( `Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// var str = "hello";
+// var res = str.toUpperCase();
+
+    // const mysec = "ankita";
+    // console.log(mysec.touppercase);
+
+    const gameName = new String('Ankita');
+    console.log(gameName.__proto__);
+    console.log(gameName.length);
+    console.log(gameName.toUpperCase());
+    console.log(gameName.charAt(2));
+    console.log(gameName.indexOf('k'));
