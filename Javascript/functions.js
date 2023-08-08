19fd450c381915ab9b42cbcda4b97f3a94c17d68
Ankitadevatale2305 <126754673+Ankitadevatale2305@users.noreply.github.com>
2023-08-08 14:15:45 +0000

    my functions

--- Javascript/functions.js
+++ Javascript/functions.js
@@ -39,8 +39,40 @@ const result = addTwoNumbers(6, 5);
 // console.log(loginUserMessage());
 
 
-function loginUserMessage(username){
+function loginUserMessage(username = "Admin level"){
+    if(!username){
+        console.log("please enter user name");
+        return
+    }
     return `${username} just logged in`
 }
-console.log(loginUserMessage("ankita"))
+//console.log(loginUserMessage())
 
+//shopping cart
+
+function calculateCartPrice(val1 , val2 , ...num1){
+    return num1
+}
+console.log(calculateCartPrice(100,200,300,400));
+
+ const user = {
+    username: "ankita" ,
+    price: 1000
+ }
+
+ function handleUserobject(anyobject){
+   console.log(`this is a username ${anyobject.username} and  this is a price ${anyobject.price}`)
+ }
+ //handleUserobject(user);
+
+ handleUserobject({
+    username: "sam" ,
+    price: 5000
+ })
+ 
+ //const myNewArray = [1100 ,2200 , 3300, 4400];
+ function returnSecondValue(getArray){
+    return getArray[1]
+ }
+// console.log(myNewArray);
+ console.log(returnSecondValue([1100 ,2200 , 3300, 4400]))
